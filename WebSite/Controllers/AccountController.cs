e31d2b46dd6b8df1d8b1c6a31a607cb1445e2a1c
ALN <00133205@me.mcu.edu.tw>
2021-08-26 14:12:29 +0800

    Login View Finish加入專案檔案。

---
+++ WebSite/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSite.Models;
+
+namespace WebSite.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Signin(string returnUrl)
+        {
+            await Task.Yield();
+            var model = new SigninViewModel
+            {
+                //登入後要轉跳的頁面
+                ReturnUrl = returnUrl,
+            };
+            return View(model);
+        }
+    }
+}
---
+++ WebSite/Models/SigninViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WebSite.Models
+{
+    public class SigninViewModel
+    {
+        [Display(Name = "帳號")]
+        [Required(ErrorMessage = "帳號必填")]
+        public string Account { get; set; }
+        [Display(Name = "密碼")]
+        [Required(ErrorMessage = "密碼必填")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
